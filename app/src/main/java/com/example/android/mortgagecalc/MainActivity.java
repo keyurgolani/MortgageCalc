bdc4f1a3c2ac49f4eefb41370f020132cbdf7179
Keyur Golani <keyurrgolani@gmail.com>
2017-03-16 23:49:45 -0700

    Initial Project Structure

---
+++ app/src/main/java/com/example/android/mortgagecalc/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.android.mortgagecalc;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mCalcFrame;
+    private FrameLayout mMyMortFrame;
+    private static final int CALC_FRAGMENT = -1;
+    private static final int MORT_FRAGMENT = 1;
+    FragmentManager fm;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_calc:
+                    setFragment(CALC_FRAGMENT);
+                    return true;
+                case R.id.navigation_my_mort:
+                    setFragment(MORT_FRAGMENT);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fm = getFragmentManager();
+        setFragment(0);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void setFragment(int fragment_id) {
+        if(fragment_id == CALC_FRAGMENT) {
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.tab, new CalcFragment());
+            ft.commit();
+        } else if(fragment_id == MORT_FRAGMENT) {
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.tab, new MortFragment());
+            ft.commit();
+        } else {
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(R.id.tab, new CalcFragment());
+            ft.commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/android/mortgagecalc/MortFragment.java
@@ -0,0 +1,21 @@
+package com.example.android.mortgagecalc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by keyurgolani on 3/16/17.
+ */
+
+public class MortFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mort_layout, container, false);
+    }
+}
