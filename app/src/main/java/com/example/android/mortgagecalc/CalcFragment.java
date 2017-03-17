92d3f28a8f851dd620aa91d87f316b3dff8f4a97
Keyur Golani <keyurrgolani@gmail.com>
2017-03-17 02:23:15 -0700

    Trying Swipe

--- app/src/main/java/com/example/android/mortgagecalc/CalcFragment.java
+++ app/src/main/java/com/example/android/mortgagecalc/CalcFragment.java
@@ -4,18 +4,71 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.example.android.utility.SimpleGestureFilter;
 
 /**
  * Created by keyurgolani on 3/16/17.
  */
 
-public class CalcFragment extends Fragment {
+public class CalcFragment extends Fragment implements SimpleGestureFilter.SimpleGestureListener{
+
+    TabHost mDetailsTab;
+    private SimpleGestureFilter detector;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calc_layout, container, false);
+        View rootView = inflater.inflate(R.layout.calc_layout, container, false);
+        mDetailsTab = (TabHost) rootView.findViewById(R.id.details_tab);
+        mDetailsTab.setup();
+
+        TabHost.TabSpec spec = mDetailsTab.newTabSpec("Loan Info");
+        spec.setContent(R.id.loan_tab);
+        spec.setIndicator("Loan Info");
+        mDetailsTab.addTab(spec);
+
+        spec = mDetailsTab.newTabSpec("Property Info");
+        spec.setContent(R.id.property_tab);
+        spec.setIndicator("Property Info");
+        mDetailsTab.addTab(spec);
+
+        detector = new SimpleGestureFilter(this.getActivity(),this);
+
+        return rootView;
+    }
+
+    public void dispatchTouchEvent(MotionEvent me){
+        // Call onTouchEvent of SimpleGestureFilter class
+        this.detector.onTouchEvent(me);
+    }
+
+    @Override
+    public void onSwipe(int direction) {
+        String str = "";
+
+        switch (direction) {
+
+            case SimpleGestureFilter.SWIPE_RIGHT : str = "Swipe Right";
+                break;
+            case SimpleGestureFilter.SWIPE_LEFT :  str = "Swipe Left";
+                break;
+            case SimpleGestureFilter.SWIPE_DOWN :  str = "Swipe Down";
+                break;
+            case SimpleGestureFilter.SWIPE_UP :    str = "Swipe Up";
+                break;
+
+        }
+        Toast.makeText(this.getActivity(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDoubleTap() {
+        Toast.makeText(this.getActivity(), "Double Tap", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/mortgagecalc/MainActivity.java
+++ app/src/main/java/com/example/android/mortgagecalc/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -39,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent me){
+        ((CalcFragment) fm.findFragmentByTag("active")).dispatchTouchEvent(me);
+        return super.dispatchTouchEvent(me);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -53,15 +60,15 @@ public class MainActivity extends AppCompatActivity {
     private void setFragment(int fragment_id) {
         if(fragment_id == CALC_FRAGMENT) {
             FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.tab, new CalcFragment());
+            ft.replace(R.id.tab, new CalcFragment(), "active");
             ft.commit();
         } else if(fragment_id == MORT_FRAGMENT) {
             FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.tab, new MortFragment());
+            ft.replace(R.id.tab, new MortFragment(), "active");
             ft.commit();
         } else {
             FragmentTransaction ft = fm.beginTransaction();
-            ft.add(R.id.tab, new CalcFragment());
+            ft.add(R.id.tab, new CalcFragment(), "active");
             ft.commit();
         }
     }
